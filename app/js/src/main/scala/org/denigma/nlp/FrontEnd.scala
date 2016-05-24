d726845a5904f53ace13519610541f5b136c2de6
antonkulaga <antonkulaga@gmail.com>
2016-05-24 14:53:07 +0200

    brat facade added

--- app/js/src/main/scala/org/denigma/nlp/FrontEnd.scala
+++ app/js/src/main/scala/org/denigma/nlp/FrontEnd.scala
@@ -5,7 +5,7 @@ import org.denigma.binding.extensions.sq
 import org.denigma.binding.views.BindableView
 import org.denigma.controls.code.CodeBinder
 import org.denigma.controls.login.{AjaxSession, LoginView}
-import org.denigma.nlp.brat.{BratUtil, ColData, DocData, EntityType}
+import org.denigma.nlp.brat._
 import org.scalajs.dom
 import org.scalajs.dom.UIEvent
 import org.scalajs.dom.raw.{Element, HTMLElement}
@@ -34,8 +34,52 @@ object FrontEnd extends BindableView with scalajs.js.JSApp
       |Our results indicate that the transphosphorylation of an endogenous epidermal growth factor receptor (EGFR) in the human embryonic kidney (HEK-293) cell line does not occur when co-expressed delta-ORs are stimulated by the delta-opioid agonist, D-Ser-Leu-enkephalin-Thr (DSLET). Moreover, neither pre-incubation of cultures with the selective EGFR antagonist, AG1478, nor down-regulation of the EGFR to a point where EGF could no longer activate ERKs had an inhibitory effect on ERK activation by DSLET. These results appear to rule out any structural or catalytic role for the EGFR in the delta-opioid-mediated MAPK cascade. To confirm these results, we used C6 glioma cells, a cell line devoid of the EGFR. In delta-OR-expressing C6 glioma cells, opioids produce a robust phosphorylation of ERK 1 and 2, whereas EGF has no stimulatory effect. Furthermore, antagonists to the RTKs that are endogenously expressed in C6 glioma cells (insulin receptor (IR) and platelet-derived growth factor receptor (PDGFR)) were unable to reduce opioid-mediated ERK activation.
     """.stripMargin
 
+  protected def initColdData(): ColData = {
+    val types =  List( new EntityType("Person", Array("Person", "Per"), "#7fa2ff", "darken") )
+    val relationTypes = List(
+      new RelationType("Anaphora", List("Anaphora", "Ana"),"3,3", "purple",
+        List(
+          new RelationRole("Anaphor", List("Person")),
+          new RelationRole("Entity", List("Person"))
+        )
+      )
+    )
+    val events  = List(
+      new EventType("Assassination", List("Assassination", "Assas"), "lightgreen", "darken",
+        List(
+          new LabeledType("Victim", List("Victim", "Vict")),
+          new LabeledType("Perpetrator", List("Perpetrator", "Perp"))
+        ))
+    )
+    val typeAttributes = List(new EntityAttributeType("Notorious", """{ "Notorious": { "glyph": "★" } }"""))
+    new ColData(  types, relationTypes, typeAttributes, events)
+  }
+
+  protected def initDocData(): DocData = {
+    val text = "Ed O'Kelley was the man who shot the man who shot Jesse James."
+    val parts = List(
+      Entity("T1", "Person", List((0, 11))),
+      Entity("T2", "Person", List((20, 23))),
+      Entity("T3", "Person", List((37, 40))),
+      Entity("T4", "Person", List((50, 61)))
+    )
+    val atribs =  List(DocAttribute("A1", "Notorious", "T4"))
+    val relations = List(Relation("R1", "Anaphora", "Anaphor", "T2", "Entity", "T1"))
+    val events = List(
+      BratEvent("E1", "T5", List("Perpetrator"->"T3", "Victim"->"T4")),
+      BratEvent("E2", "T6", List("Perpetrator"->"T2", "Victim"->"T3"))
+    )
+    val triggers = List(
+      Entity("T5", "Assassination", List((45, 49))),
+      Entity("T6", "Assassination", List((28, 32)))
+    )
+    new DocData( text, parts, atribs, relations, events, triggers)
+  }
+
   @JSExport
   def main(): Unit = {
+    import scalajs.js.JSConverters._
+
     this.bindView()
     val bratLocation = "/resources/brat"
     val webFontURLs = Array(
@@ -43,148 +87,10 @@ object FrontEnd extends BindableView with scalajs.js.JSApp
     bratLocation + "/static/fonts/PT_Sans-Caption-Web-Regular.ttf",
     bratLocation + "/static/fonts/Liberation_Sans-Regular.ttf"
     )
-
-    val colData = new ColData(
-      Array(
-        new EntityType("Person", Array("Person", "Per"), "#7fa2ff", "darken")
-      )
-    )
-    val docData = new DocData(
-      "Ed O'Kelley was the man who shot the man who shot Jesse James.",
-      Array(
-        /* Format: [${ID}, ${TYPE}, [[${START}, ${END}]]]
-        note that range of the offsets are [${START},${END}) */
-        Array("T1", "Person", Array(Array(0, 11))),
-        Array("T2", "Person", Array(Array(20, 23))),
-        Array("T3", "Person", Array(Array(37, 40))),
-        Array("T4", "Person", Array(Array(50, 61)))
-    ))
-    BratUtil.embed("annotation", colData, docData, webFontURLs)
+    val colData = initColdData()
+    val docData = initDocData()
+    BratUtil.embed("annotation", colData, docData, webFontURLs.toJSArray)
     println("LET US LOQD IN A NORMAL WAY")
-/*
-    js.eval(
-      """
-        |var bratLocation = '/resources/brat';
-        |        var webFontURLs = [
-        |          bratLocation + '/static/fonts/Astloch-Bold.ttf',
-        |          bratLocation + '/static/fonts/PT_Sans-Caption-Web-Regular.ttf',
-        |          bratLocation + '/static/fonts/Liberation_Sans-Regular.ttf'
-        |        ];
-        |
-        |
-        |        var collData = {
-        |          entity_types: [ {
-        |            type   : 'Person',
-        |            /* The labels are used when displaying the annotion, in this case
-        |             we also provide a short-hand "Per" for cases where
-        |             abbreviations are preferable */
-        |            labels : ['Person', 'Per'],
-        |            // Blue is a nice colour for a person?
-        |            bgColor: '#7fa2ff',
-        |            // Use a slightly darker version of the bgColor for the border
-        |            borderColor: 'darken'
-        |          } ]
-        |        };
-        |
-        |        var docData = {
-        |          // Our text of choice
-        |          text     : "Ed O'Kelley was the man who shot the man who shot Jesse James.",
-        |          // The entities entry holds all entity annotations
-        |          entities : [
-        |            /* Format: [${ID}, ${TYPE}, [[${START}, ${END}]]]
-        |             note that range of the offsets are [${START},${END}) */
-        |            ['T1', 'Person', [[0, 11]]],
-        |            ['T2', 'Person', [[20, 23]]],
-        |            ['T3', 'Person', [[37, 40]]],
-        |            ['T4', 'Person', [[50, 61]]],
-        |          ],
-        |        };
-        |
-        |        collData['entity_attribute_types'] = [ {
-        |          type  : 'Notorious',
-        |          /* brat supports multi-valued attributes, but in our case we will only
-        |           use a single value and add a glyph to the visualisation to indicate
-        |           that the entity carries that attribute */
-        |          values: { 'Notorious': { 'glyph': '★' } }
-        |        } ];
-        |
-        |
-        |        docData['attributes'] = [
-        |          // Format: [${ID}, ${TYPE}, ${TARGET}]
-        |          ['A1', 'Notorious', 'T4']
-        |        ];
-        |
-        |
-        |        collData['relation_types'] = [ {
-        |          type     : 'Anaphora',
-        |          labels   : ['Anaphora', 'Ana'],
-        |          // dashArray allows you to adjust the style of the relation arc
-        |          dashArray: '3,3',
-        |          color    : 'purple',
-        |          /* A relation takes two arguments, both are named and can be constrained
-        |           as to which types they may apply to */
-        |          args     : [
-        |            //
-        |            {role: 'Anaphor', targets: ['Person'] },
-        |            {role: 'Entity',  targets: ['Person'] }
-        |          ]
-        |        } ];
-        |
-        |        docData['relations'] = [
-        |          // Format: [${ID}, ${TYPE}, [[${ARGNAME}, ${TARGET}], [${ARGNAME}, ${TARGET}]]]
-        |          ['R1', 'Anaphora', [['Anaphor', 'T2'], ['Entity', 'T1']]]
-        |        ];
-        |
-        |
-        |        collData['event_types'] = [ {
-        |          type   : 'Assassination',
-        |          labels : ['Assassination', 'Assas'],
-        |          bgColor: 'lightgreen',
-        |          borderColor: 'darken',
-        |          /* Unlike relations, events originate from a span of text and can take
-        |           several arguments */
-        |          arcs   : [
-        |            {type: 'Victim', labels: ['Victim','Vict'] },
-        |            // Just like the event itself, its arguments can be styled
-        |            {type: 'Perpetrator', labels: ['Perpetrator','Perp'], color: 'green' }
-        |          ]
-        |        } ];
-        |
-        |        /* Events also have trigger annotations, these are spans that are not
-        |         visualised. This enables sharing of triggers when this is desirable, such
-        |         as in the sentence "He robbed the bank and the post office", where
-        |         "robbed" gives rice to two separate events that shares a single trigger */
-        |        docData['triggers'] = [
-        |          // The format is identical to that of entities
-        |          ['T5', 'Assassination', [[45, 49]]],
-        |          ['T6', 'Assassination', [[28, 32]]]
-        |        ];
-        |
-        |        docData['events'] = [
-        |          // Format: [${ID}, ${TRIGGER}, [[${ARGTYPE}, ${ARGID}], ...]]
-        |          ['E1', 'T5', [['Perpetrator', 'T3'], ['Victim', 'T4']]],
-        |          ['E2', 'T6', [['Perpetrator', 'T2'], ['Victim', 'T3']]]
-        |        ];
-        |
-        |
-        |        $( document ).ready(function() {
-        |          Util.embed(
-        |                  // id of the div element where brat should embed the visualisations
-        |                  'annotation',
-        |                  // object containing collection data
-        |                  collData,
-        |                  // object containing document data
-        |                  docData,
-        |                  // Array containing locations of the visualisation fonts
-        |                  webFontURLs
-        |          );
-        |        });
-        |
-        |
-        |
-      """.stripMargin
-    )
-    */
 
     connector.open()
     connector.send(MessagesNLP.Annotate(text))
--- app/js/src/main/scala/org/denigma/nlp/brat/Util.scala
+++ app/js/src/main/scala/org/denigma/nlp/brat/Util.scala
@@ -1,20 +1,148 @@
 package org.denigma.nlp.brat
 
 import scala.scalajs.js
+import scala.scalajs.js.Array
 import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
+import scalajs.js.JSConverters._
 
 @JSName("Util")
 @js.native
 object BratUtil  extends js.Object {
 
-  def embed(id: String = "annotation", collData: ColData, docData: DocData,  webFontURLs: Array[String]): Unit = js.native
+  def embed(id: String = "annotation", collData: ColData, docData: DocData,  webFontURLs: js.Array[String]): Unit = js.native
 }
 
 @ScalaJSDefined
-class ColData(val entity_types: Array[EntityType]) extends js.Object
+class RelationRole(val role: String, ts: List[String]) extends js.Object{
+  val targets = ts.toJSArray
+}
+
+/*
+collData['event_types'] = [ {
+    type   : 'Assassination',
+    labels : ['Assassination', 'Assas'],
+    bgColor: 'lightgreen',
+    borderColor: 'darken',
+    /* Unlike relations, events originate from a span of text and can take
+        several arguments */
+    arcs   : [
+        {type: 'Victim', labels: ['Victim','Vict'] },
+        // Just like the event itself, its arguments can be styled
+        {type: 'Perpetrator', labels: ['Perpetrator','Perp'], color: 'green' }
+    ]
+} ];
+ */
 
 @ScalaJSDefined
-class EntityType(val `type`: String, val labels: Array[String], val bgColor: String, val borderColor: String) extends js.Object
+class RelationType(val `type`: String, lbs: List[String], val dashArray: String = "3,3", val color: String = "purple", roles: List[RelationRole]) extends js.Object {
+
+  val labels = lbs.toJSArray
+
+  val args = roles.toJSArray
+
+}
+@ScalaJSDefined
+class LabeledType(val `type`: String, lbs: List[String], val color: String = "") extends js.Object {
+
+  val labels = lbs.toJSArray
+}
+
 
 @ScalaJSDefined
-class DocData(val text: String, val entities: Array[_]) extends js.Object
+class EventType(val `type`: String, lbs: List[String], val bgColor: String = "lightgreen", borderColor: String = "darken", types: List[LabeledType]) extends js.Object {
+  val labels: Array[String] = lbs.toJSArray
+  val arcs = types.toJSArray
+/*
+ arcs   : [
+        {type: 'Victim', labels: ['Victim','Vict'] },
+        // Just like the event itself, its arguments can be styled
+        {type: 'Perpetrator', labels: ['Perpetrator','Perp'], color: 'green' }
+    ]
+ */
+}
+
+@ScalaJSDefined
+class ColData(types: List[EntityType], relationTypes: List[RelationType] = Nil, attributes: List[EntityAttributeType] = Nil, events: List[EventType]) extends js.Object
+{
+
+  val entity_types: js.Array[EntityType] =  types.toJSArray
+
+  val entity_attribute_types = attributes.toJSArray
+
+  val relation_types: Array[RelationType] = relationTypes.toJSArray
+
+  val event_types = events.toJSArray
+
+  println(js.JSON.stringify(event_types))
+
+}
+
+@ScalaJSDefined
+class EntityType(val `type`: String, lbs: Seq[String], val bgColor: String, val borderColor: String) extends js.Object
+{
+  val labels: js.Array[String]=  lbs.toJSArray
+}
+
+case class Entity(id: String, tp: String, pairs: List[(Int, Int)])
+{
+  //[${ID}, ${TYPE}, [[${START}, ${END}]]
+  lazy val toJSArray = {
+    val chunks = pairs.map{case (from, to)=> js.Array(from, to)}
+    js.Array(id, tp, chunks.toJSArray)
+  }
+}
+
+case class DocAttribute(id: String, tp: String, target: String) {
+
+  // Format: [${ID}, ${TYPE}, ${TARGET}]
+  // ['A1', 'Notorious', 'T4']
+  lazy val toJSArray = js.Array(id, tp, target)
+
+}
+
+
+@ScalaJSDefined
+class EntityAttributeType(val `type`: String, valueStr: String) extends js.Object{
+
+  val values = scalajs.js.JSON.parse(valueStr)
+
+}
+/*
+*  // Format: [${ID}, ${TYPE}, [[${ARGNAME}, ${TARGET}], [${ARGNAME}, ${TARGET}]]]
+    ['R1', 'Anaphora', [['Anaphor', 'T2'], ['Entity', 'T1']]]
+* */
+case class Relation(id: String, tp: String, arg1: String, target1: String, arg2: String, target2: String) {
+  lazy val toJSArray: Array[Object] = js.Array(id, tp, js.Array(js.Array(arg1, target1),js.Array(arg2, target2)))
+}
+
+case class BratEvent(id: String, trigger: String, args: List[(String, String)]) {
+
+  val arguments: Array[Array[String]] = args.map{case (key, value)=> js.Array(key, value)}.toJSArray
+
+  // Format: [${ID}, ${TRIGGER}, [[${ARGTYPE}, ${ARGID}], ...]]
+  //['E1', 'T5', [['Perpetrator', 'T3'], ['Victim', 'T4']]],
+  lazy val toJSArray = js.Array(id, trigger, arguments)
+}
+
+@ScalaJSDefined
+class DocData(val text: String,
+              elements: List[Entity],
+              attribs: List[DocAttribute] = Nil,
+              rels: List[Relation] = Nil,
+              evs: List[BratEvent] = Nil,
+              trigs: List[Entity] = Nil
+             ) extends js.Object
+{
+  val entities: Array[Array[Object]] = elements.map(p=>p.toJSArray).toJSArray
+
+  val attributes = attribs.map(_.toJSArray).toJSArray
+
+  val relations = rels.map(r=>r.toJSArray).toJSArray
+
+  val events: Array[Array[Object]] = evs.map(ev=>ev.toJSArray).toJSArray
+  println(js.JSON.stringify(events))
+
+  val triggers: Array[Array[Object]] = trigs.map(p=>p.toJSArray).toJSArray
+
+
+}
