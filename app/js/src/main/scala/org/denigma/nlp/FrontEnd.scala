046664d0f8ebf1ad3ea3053bb23c18983b785770
antonkulaga <antonkulaga@gmail.com>
2016-05-24 09:16:21 +0200

    facade construction stastarted

--- app/js/src/main/scala/org/denigma/nlp/FrontEnd.scala
+++ app/js/src/main/scala/org/denigma/nlp/FrontEnd.scala
@@ -5,11 +5,13 @@ import org.denigma.binding.extensions.sq
 import org.denigma.binding.views.BindableView
 import org.denigma.controls.code.CodeBinder
 import org.denigma.controls.login.{AjaxSession, LoginView}
+import org.denigma.nlp.brat.{BratUtil, ColData, DocData, EntityType}
 import org.scalajs.dom
 import org.scalajs.dom.UIEvent
 import org.scalajs.dom.raw.{Element, HTMLElement}
 import rx.Ctx.Owner.Unsafe.Unsafe
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
 @JSExport("FrontEnd")
 object FrontEnd extends BindableView with scalajs.js.JSApp
@@ -35,6 +37,155 @@ object FrontEnd extends BindableView with scalajs.js.JSApp
   @JSExport
   def main(): Unit = {
     this.bindView()
+    val bratLocation = "/resources/brat"
+    val webFontURLs = Array(
+    bratLocation + "/static/fonts/Astloch-Bold.ttf",
+    bratLocation + "/static/fonts/PT_Sans-Caption-Web-Regular.ttf",
+    bratLocation + "/static/fonts/Liberation_Sans-Regular.ttf"
+    )
+
+    val colData = new ColData(
+      Array(
+        new EntityType("Person", Array("Person", "Per"), "#7fa2ff", "darken")
+      )
+    )
+    val docData = new DocData(
+      "Ed O'Kelley was the man who shot the man who shot Jesse James.",
+      Array(
+        /* Format: [${ID}, ${TYPE}, [[${START}, ${END}]]]
+        note that range of the offsets are [${START},${END}) */
+        Array("T1", "Person", Array(Array(0, 11))),
+        Array("T2", "Person", Array(Array(20, 23))),
+        Array("T3", "Person", Array(Array(37, 40))),
+        Array("T4", "Person", Array(Array(50, 61)))
+    ))
+    BratUtil.embed("annotation", colData, docData, webFontURLs)
+    println("LET US LOQD IN A NORMAL WAY")
+/*
+    js.eval(
+      """
+        |var bratLocation = '/resources/brat';
+        |        var webFontURLs = [
+        |          bratLocation + '/static/fonts/Astloch-Bold.ttf',
+        |          bratLocation + '/static/fonts/PT_Sans-Caption-Web-Regular.ttf',
+        |          bratLocation + '/static/fonts/Liberation_Sans-Regular.ttf'
+        |        ];
+        |
+        |
+        |        var collData = {
+        |          entity_types: [ {
+        |            type   : 'Person',
+        |            /* The labels are used when displaying the annotion, in this case
+        |             we also provide a short-hand "Per" for cases where
+        |             abbreviations are preferable */
+        |            labels : ['Person', 'Per'],
+        |            // Blue is a nice colour for a person?
+        |            bgColor: '#7fa2ff',
+        |            // Use a slightly darker version of the bgColor for the border
+        |            borderColor: 'darken'
+        |          } ]
+        |        };
+        |
+        |        var docData = {
+        |          // Our text of choice
+        |          text     : "Ed O'Kelley was the man who shot the man who shot Jesse James.",
+        |          // The entities entry holds all entity annotations
+        |          entities : [
+        |            /* Format: [${ID}, ${TYPE}, [[${START}, ${END}]]]
+        |             note that range of the offsets are [${START},${END}) */
+        |            ['T1', 'Person', [[0, 11]]],
+        |            ['T2', 'Person', [[20, 23]]],
+        |            ['T3', 'Person', [[37, 40]]],
+        |            ['T4', 'Person', [[50, 61]]],
+        |          ],
+        |        };
+        |
+        |        collData['entity_attribute_types'] = [ {
+        |          type  : 'Notorious',
+        |          /* brat supports multi-valued attributes, but in our case we will only
+        |           use a single value and add a glyph to the visualisation to indicate
+        |           that the entity carries that attribute */
+        |          values: { 'Notorious': { 'glyph': '★' } }
+        |        } ];
+        |
+        |
+        |        docData['attributes'] = [
+        |          // Format: [${ID}, ${TYPE}, ${TARGET}]
+        |          ['A1', 'Notorious', 'T4']
+        |        ];
+        |
+        |
+        |        collData['relation_types'] = [ {
+        |          type     : 'Anaphora',
+        |          labels   : ['Anaphora', 'Ana'],
+        |          // dashArray allows you to adjust the style of the relation arc
+        |          dashArray: '3,3',
+        |          color    : 'purple',
+        |          /* A relation takes two arguments, both are named and can be constrained
+        |           as to which types they may apply to */
+        |          args     : [
+        |            //
+        |            {role: 'Anaphor', targets: ['Person'] },
+        |            {role: 'Entity',  targets: ['Person'] }
+        |          ]
+        |        } ];
+        |
+        |        docData['relations'] = [
+        |          // Format: [${ID}, ${TYPE}, [[${ARGNAME}, ${TARGET}], [${ARGNAME}, ${TARGET}]]]
+        |          ['R1', 'Anaphora', [['Anaphor', 'T2'], ['Entity', 'T1']]]
+        |        ];
+        |
+        |
+        |        collData['event_types'] = [ {
+        |          type   : 'Assassination',
+        |          labels : ['Assassination', 'Assas'],
+        |          bgColor: 'lightgreen',
+        |          borderColor: 'darken',
+        |          /* Unlike relations, events originate from a span of text and can take
+        |           several arguments */
+        |          arcs   : [
+        |            {type: 'Victim', labels: ['Victim','Vict'] },
+        |            // Just like the event itself, its arguments can be styled
+        |            {type: 'Perpetrator', labels: ['Perpetrator','Perp'], color: 'green' }
+        |          ]
+        |        } ];
+        |
+        |        /* Events also have trigger annotations, these are spans that are not
+        |         visualised. This enables sharing of triggers when this is desirable, such
+        |         as in the sentence "He robbed the bank and the post office", where
+        |         "robbed" gives rice to two separate events that shares a single trigger */
+        |        docData['triggers'] = [
+        |          // The format is identical to that of entities
+        |          ['T5', 'Assassination', [[45, 49]]],
+        |          ['T6', 'Assassination', [[28, 32]]]
+        |        ];
+        |
+        |        docData['events'] = [
+        |          // Format: [${ID}, ${TRIGGER}, [[${ARGTYPE}, ${ARGID}], ...]]
+        |          ['E1', 'T5', [['Perpetrator', 'T3'], ['Victim', 'T4']]],
+        |          ['E2', 'T6', [['Perpetrator', 'T2'], ['Victim', 'T3']]]
+        |        ];
+        |
+        |
+        |        $( document ).ready(function() {
+        |          Util.embed(
+        |                  // id of the div element where brat should embed the visualisations
+        |                  'annotation',
+        |                  // object containing collection data
+        |                  collData,
+        |                  // object containing document data
+        |                  docData,
+        |                  // Array containing locations of the visualisation fonts
+        |                  webFontURLs
+        |          );
+        |        });
+        |
+        |
+        |
+      """.stripMargin
+    )
+    */
+
     connector.open()
     connector.send(MessagesNLP.Annotate(text))
   }
--- app/js/src/main/scala/org/denigma/nlp/brat/Util.scala
+++ app/js/src/main/scala/org/denigma/nlp/brat/Util.scala
@@ -1,11 +1,20 @@
 package org.denigma.nlp.brat
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.annotation.{JSName, ScalaJSDefined}
 
 @JSName("Util")
 @js.native
-object Util  extends js.Object {
+object BratUtil  extends js.Object {
 
-  def embed(id: String = "annotation", collData: Any, docData: Any,  webFontURLs: Array[String]) = js.native
+  def embed(id: String = "annotation", collData: ColData, docData: DocData,  webFontURLs: Array[String]): Unit = js.native
 }
+
+@ScalaJSDefined
+class ColData(val entity_types: Array[EntityType]) extends js.Object
+
+@ScalaJSDefined
+class EntityType(val `type`: String, val labels: Array[String], val bgColor: String, val borderColor: String) extends js.Object
+
+@ScalaJSDefined
+class DocData(val text: String, val entities: Array[_]) extends js.Object
