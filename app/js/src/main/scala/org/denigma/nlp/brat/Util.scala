3fcd8c3b15bff30bb73c914e0c5b50cee6092311
antonkulaga <antonkulaga@gmail.com>
2016-05-24 02:48:26 +0200

    changed brat

---
+++ app/js/src/main/scala/org/denigma/nlp/brat/Util.scala
@@ -0,0 +1,11 @@
+package org.denigma.nlp.brat
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@JSName("Util")
+@js.native
+object Util  extends js.Object {
+
+  def embed(id: String = "annotation", collData: Any, docData: Any,  webFontURLs: Array[String]) = js.native
+}
